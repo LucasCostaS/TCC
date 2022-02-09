2859163cd5e07546ec414ca8a6b5ea9bb118e409
LucasCostaS <lcs.c.sza@gmail.com>
2022-02-09 08:57:42 -0300

    Script troca de cor
    codigo de troca de cor funcionando

--- Assets/Scripts/Rotacao.cs
+++ Assets/Scripts/Rotacao.cs
@@ -5,32 +5,27 @@ using UnityEngine;
 public class Rotacao : MonoBehaviour
 {
 
-    private Renderer rend;
+    private SpriteRenderer rend;
 
     private void OnMouseOver()
     {
-        rend = GetComponent<Renderer>();
-        rend.material.color = Color.yellow;
+        rend = GetComponent<SpriteRenderer>();
+        rend.color = Color.yellow;
         // Se botão direito do mouse pressionado, rotaciona para direita
         if (Input.GetMouseButtonDown(0))
         {
-
-
             transform.Rotate(0.0f, 0.0f, -45.0f, Space.Self);
         }
         // Se botão direito do mouse pressionado, rotaciona para esquerda
         else if (Input.GetMouseButtonDown(1))
         {
-            Debug.Log(Input.mousePosition);
             transform.Rotate(0.0f, 0.0f, 45.0f, Space.Self);
         }
     }
 
     private void OnMouseExit()
     {
-
-        rend.material.color = Color.black;
-
+        rend.color = Color.black;
     }
 
 }
